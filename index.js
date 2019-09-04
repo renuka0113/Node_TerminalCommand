a99d5841f3159c294f413657ea8ed23d14740f73
Renuka <renukaalurkar@gmail.com>
2019-09-04 13:42:04 -0500

    touch completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const terminalCommands=require('./terminal-commands');
 terminalCommands.ls();
 terminalCommands.mkdir();
+terminalCommands.touch();
--- terminal-commands.js
+++ terminal-commands.js
@@ -1,4 +1,5 @@
 const fs=require("fs");
+var now=new Date();
 module.exports.ls=()=>{
  fs.readdir('./',(err,files)=>{
   const filesToString=files.reduce((acc,file)=>{
@@ -12,6 +13,13 @@ module.exports.mkdir = () => {
   fs.mkdir('C:/Users/rally/Downloads/NODE/Node_TerminalCommand/new_dir', { recursive: true }, (err) => {
   if (err){
     return console.log(`You have an error:  ${err}`);
+  }
+});
+};
+module.exports.touch = () => {
+  fs.openSync('C:/Users/rally/Downloads/NODE/Node_TerminalCommand/message.txt', 'w', (err) => {
+  if (err) {
+    return console.log(`You have an error: ${err}`);
   }
 });
 };
